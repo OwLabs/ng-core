686b31a63015ef1510b4e1a5251bcd40d95beaca
Ahmad Haziq <ahmadhaziq2600@gmail.com>
2026-03-11 10:14:27 +0800

    feat(otp): add verify user command and handlers

--- src/modules/users/application/commands/handlers/index.ts
+++ src/modules/users/application/commands/handlers/index.ts
@@ -1,2 +1,3 @@
 export * from './create-user.handler';
 export * from './update-user-roles.handler';
+export * from './verify-user.handler';
--- src/modules/users/application/commands/impl/index.ts
+++ src/modules/users/application/commands/impl/index.ts
@@ -1,2 +1,3 @@
 export * from './create-user.command';
 export * from './update-user-roles.command';
+export * from './verify-user.command';
